1afab0982a21ca9f7f01f0beebdded572505baf2
Divyam Nandanwar <divyam.nandanwar@infogain.com>
2018-08-17 10:41:10 +0530

    category list tests done

--- src/app/category-list/category-list.component.spec.ts
+++ src/app/category-list/category-list.component.spec.ts
@@ -14,7 +14,6 @@ import { Router } from '@angular/router';
 describe('CategoryListComponent', () => {
   let component: CategoryListComponent;
   let fixture: ComponentFixture<CategoryListComponent>;
-  let router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -35,7 +34,6 @@ describe('CategoryListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CategoryListComponent);
     component = fixture.componentInstance;
-    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
@@ -64,15 +62,14 @@ describe('CategoryListComponent', () => {
     expect(matchips.length).toBeLessThanOrEqual(7);
   })));
 
-  // it('should navigate according to category', fakeAsync(inject([SearchingService], (service: SearchingService) => {
-  //   const navigateSpy = spyOn(router, 'navigate');
-  //   spyOn(service, 'getAllCategories').and.returnValue(of([{id: 0, name: 'play'}]));
-  //   component.ngOnInit();
-  //   fixture.detectChanges();
-  //   const chip = fixture.debugElement.queryAll(By.css('mat-chip'));
-  //   chip[0].nativeElement.click();
-  //   tick();
-  //   fixture.detectChanges();
-  //   expect(navigateSpy).toHaveBeenCalledWith(['/category/play']);
-  // })));
+  it('should navigate according to category', inject([SearchingService], (service: SearchingService) => {
+    const navigateSpy = spyOn(component, 'goto');
+    spyOn(service, 'getAllCategories').and.returnValue(of([{id: 0, name: 'play'}]));
+    component.ngOnInit();
+    fixture.detectChanges();
+    const chip = fixture.debugElement.queryAll(By.css('mat-chip'));
+    chip[0].nativeElement.click();
+    fixture.detectChanges();
+    expect(navigateSpy).toHaveBeenCalled();
+  }));
 });
